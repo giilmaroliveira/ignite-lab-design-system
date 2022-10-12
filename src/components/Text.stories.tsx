a7aa61ed6bb78dfb803f7122f21a2914e59ed587
Gilmar Oliveira <giilmaroliveira@outlook.com>
2022-10-12 15:32:34 -0300

    Adding Heading Component

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,42 +6,47 @@ export default {
   component: Text,
   args: {
     children: 'Lorem ipsum.',
-    size: 'md'
+    size: 'md',
   },
   argTypes: {
     size: {
       options: ['sm', 'md', 'lg'],
       control: {
-        type: 'inline-radio'
-      }
-    }
-  }
+        type: 'inline-radio',
+      },
+    },
+  },
 } as Meta<TextProps>;
 
 export const Default: StoryObj<TextProps> = {};
 
 export const Small: StoryObj<TextProps> = {
-    args: {
-        size: 'sm'
-    }
+  args: {
+    size: 'sm',
+  },
 };
 
 export const Large: StoryObj<TextProps> = {
-    args: {
-        size: 'lg'
-    }
+  args: {
+    size: 'lg',
+  },
 };
 
 export const CustomComponent: StoryObj<TextProps> = {
-    args: {
-        asChild: true,
-        children: (<p>Testando</p>)
+  args: {
+    asChild: true,
+    children: <p>Text with P tag</p>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
     },
-    argTypes: {
-      children: {
-        table: {
-          disable: true
-        }
-      }
-    }
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
