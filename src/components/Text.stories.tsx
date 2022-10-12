ffba42937b31ec792f18f0d9a2650f051b63912e
Gilmar Oliveira <giilmaroliveira@outlook.com>
2022-10-12 15:26:02 -0300

    Storybook disable child for CustomComponent variant

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -5,7 +5,16 @@ export default {
   title: 'Components/Text',
   component: Text,
   args: {
-    children: 'Lorem ipsum.'
+    children: 'Lorem ipsum.',
+    size: 'md'
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
   }
 } as Meta<TextProps>;
 
@@ -27,5 +36,12 @@ export const CustomComponent: StoryObj<TextProps> = {
     args: {
         asChild: true,
         children: (<p>Testando</p>)
+    },
+    argTypes: {
+      children: {
+        table: {
+          disable: true
+        }
+      }
     }
 };
