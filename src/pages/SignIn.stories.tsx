7015955ae22f7855dc57259f0a8b61470dd5a0d9
Gilmar Oliveira <giilmaroliveira@outlook.com>
2022-10-16 18:25:53 -0300

    Adding and implemeting msw / msw-storybook-addon for mock requests

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, waitFor } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { rest } from 'msw';
 import { SignIn } from './SignIn';
 
 export default {
@@ -8,6 +9,15 @@ export default {
   component: SignIn,
   args: {},
   argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('/sessions', (req, res, ctx) => {
+          return res(ctx.json({ message: 'Login realizado!' }));
+        }),
+      ],
+    },
+  },
 } as Meta;
 
 export const Default: StoryObj = {
@@ -16,7 +26,7 @@ export const Default: StoryObj = {
 
     userEvent.type(
       canvas.getByPlaceholderText('Digite seu e-mail'),
-      'gilmar@gmail.com'
+      'gilmar@mail.com'
     );
     userEvent.type(canvas.getByPlaceholderText('******'), '123456');
 
